df3cafa3a55cfe301acc342f79fdd5cc67febae0
BogdanDarlau <darlau.bogdan@yahoo.com>
2023-07-16 00:04:17 +0300

    last commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "71698a11dcacffec"; }
+/******/ 		__webpack_require__.h = function() { return "a5344bbdb0deb1a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -84,7 +84,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8eb798d3d7e172d4"; }
+/******/ 	__webpack_require__.h = function() { return "7cbcd43fd5338e2b"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
